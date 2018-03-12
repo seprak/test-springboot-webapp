068df21273b16f41a52e666c0bfb3040400befdc
vladimir seprak <vladimir.seprak@gmail.com>
2018-03-12 20:26:22 +0100

    xxx

--- src/main/java/com/vsprk/example/springboot/examplewebapp/HelloController.java
+++ src/main/java/com/vsprk/example/springboot/examplewebapp/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Manny Greetings from Spring Boot!";
+        return "Manny Greetings from Spring Boot! on " + System.getProperty("os.name");
     }
 
 }
